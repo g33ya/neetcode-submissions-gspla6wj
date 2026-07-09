84260e0c500fd2f914e68dfdbb3ef04c8a3bf496
g33ya <115582758+g33ya@users.noreply.github.com>
2026-07-09 13:59:04 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,18 @@
+# Relationship between i and j
+# What is the minimum amount of information I can store to achieve one pass?
+
+# 3 + 4 = 7  -> 7 - 3 = 4
+# 3 + ? = target, target - i = ___ (j)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sums = {}
+        
+        for i in range(len(nums)):
+            if target - nums[i] in sums:
+                return [sums[target - nums[i]], i]
+            else:
+                sums[nums[i]] = i
+        
+        
+        
