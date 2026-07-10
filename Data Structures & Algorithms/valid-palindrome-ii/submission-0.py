6ad61293f292184bde5df00d856ff13f353ff40c
g33ya <115582758+g33ya@users.noreply.github.com>
2026-07-10 14:12:28 -0400

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        """
+        - use two pointer (l/r)
+        - can delete at most one character -> "skip" char with substrings, check reversal for
+            valid palindrome
+        - important: will need to check "skip" of both l/r elements (2 checks!)
+        """
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if (s[l] != s[r]):
+                skipL, skipR = s[l+1:r + 1], s[l:r]
+                return skipL == skipL[::-1] or skipR == skipR[::-1]
+            l, r = l + 1, r -1
+        return True
+
+        
