ba3d9593338b8f0d8b2c69fada22eef256e71fb6
g33ya <115582758+g33ya@users.noreply.github.com>
2026-07-12 20:39:55 -0400

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        - recursion!
+        - what is the base case?
+        - how can i split this into smaller problems?
+        - how do i give the function what remains to solve?
+        '''
+
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
