c13fea979e84777ef8c9a6629810155ea6870784
g33ya <115582758+g33ya@users.noreply.github.com>
2026-07-29 22:04:48 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        '''
+        most recently seen: stack
+        mapping? yes -> closing to opening brackets
+        when to push? opening bracket
+        when to pop? when closing bracket matches most recent open bracket
+        remember to check if stack is empty.
+        '''
+        pairs = {"}": "{", ")": "(", "]": "["}
+        stack = []
+
+        for c in s:
+            if c not in pairs:
+                stack.append(c)
+            else:
+                if not stack or stack[-1] != pairs[c]:
+                    return False
+                stack.pop()
+        return not stack
